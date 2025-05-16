31d5c86c7c1f7210817f9b3f85a1ba71dbd2b9fb
anonymous <anonymous@example.com>
2025-05-16 09:48:58 +0200

    fix: cleanup

--- src/components/verification-sheet/index.tsx
+++ src/components/verification-sheet/index.tsx
@@ -1,14 +1,15 @@
-import { ScrollArea } from 'radix-ui';
-import { QRCodeSVG } from 'qrcode.react';
-import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 import {
   CreatePresentationRequest,
   GetPresentationState,
 } from '../../lib/presentation';
+import { useQuery } from '@tanstack/react-query';
 import { SheetHeader } from './sheet-header';
+import { ScrollArea } from 'radix-ui';
+import { AgeVerification } from './age-verification';
+import { VerificationSuccess } from './verification-success';
 
-export default function VerificationSheet() {
+export function VerificationSheet() {
   const [verified, setVerified] = useState(false);
 
   const query = useQuery({
@@ -31,40 +32,12 @@ export default function VerificationSheet() {
   return (
     <>
       <SheetHeader />
-
       <ScrollArea.Root className="h-[calc(100vh-160px)] w-full overflow-auto px-8 pb-8">
-        <h4 className="text-xl font-bold text-black">
-          This film requires a proof of age
-        </h4>
-        <p className="mt-2 text-black">
-          You must be at least 18 years old to purchase cinema tickets for this
-          film. Your age will also be checked before you enter the cinema on the
-          day of the screening.
-        </p>
-        {!verified && query.data && query.data.transaction_id && (
-          <QRCodeSVG
-            value={`eudi-openid4vp://?client_id=${encodeURIComponent(
-              query.data.client_id
-            )}&request_uri=${encodeURIComponent(
-              query.data.request_uri
-            )}&request_uri_method=${encodeURIComponent(query.data.request_uri_method)}`}
-            className="h-90 w-full py-8"
-          />
+        {!verified ? (
+          <AgeVerification verified={verified} data={query.data} />
+        ) : (
+          <VerificationSuccess />
         )}
-        <h4 className="text-xl font-bold text-black">How it works:</h4>
-        <p className="mt-4 text-black">
-          1. Open the Age Verification App on your Smartphone.
-        </p>
-        <p className="text-black">2. Use the app to scan the QR code above.</p>
-        <p className="text-black">
-          3. Follow the instructions in the app to complete the verification.
-        </p>
-        <p className="mt-12 text-black">
-          Once verified, this page will refresh automatically.
-          <br /> 🔒 Your data is secure and will not be shared without your
-          consent. Don't have the app yet? Download it on Googleplay or
-          Appstore.
-        </p>
       </ScrollArea.Root>
     </>
   );
